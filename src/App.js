dbe13e7d51d5ccaf73887e30902be8e745ca7487
Rajottam Pradhan <84380744+Rajottam10@users.noreply.github.com>
2023-11-19 11:09:27 +0545

    Update App.js
    added comment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Products from "./components/pages/Products";
 import Services from "./components/pages/Services";
 import SignUp from "./components/pages/SignUp";
 
+//this is the function
 function App() {
   return (
     <>
