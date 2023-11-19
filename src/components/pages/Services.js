5360c1293cc7394a8aab6f7397242de9a0475131
Rojan <shrestharojan79@gmail.com>
2023-11-19 12:59:08 +0545

    image upload

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import '../../App.css';
+import axios from 'axios';
 
 export default function Services(){
+    const[image,setImage]=useState('');
+    function handleImage(e){
+            setImage(e.target.file[0]);
+    }
+
+    function handleApi(){
+        const formData=  new FormData();
+        formData.append('image',image);
+        axios.post('url',formData).then((res)=>{
+            console.log(res)
+        })
+    }
     return(
-        <h1 className='services'>SERVICES</h1>
+    <div>
+        <h1 className='services'>Upload Your Image Here!</h1>
+        <input type='file' name='file' onChange={handleImage}/>
+        <button onClick={handleApi}>Submit</button>
+    </div>
     )
 };
