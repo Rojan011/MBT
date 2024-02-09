4a1c2ff375b9678264916520ff3e0a0967681c3a
Rojan <shrestharojan79@gmail.com>
2024-02-09 11:59:38 +0545

    feature : profile on login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -45,6 +45,13 @@ function Navbar() {
   }, []);
 
   window.addEventListener("resize", showButton);
+
+  const getUser = () => {
+    const userString = sessionStorage.getItem("user");
+    const user_detail = JSON.parse(userString);
+    return user_detail;
+  };
+  const user = getUser();
 
   return (
     <>
@@ -95,28 +102,50 @@ function Navbar() {
               </Link>
             </li>
 
-           {!login &&  <li>
-              <Link
-                to="/sign-up"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Login
-              </Link>
-            </li>}
+            {!login && (
+              <li>
+                <Link
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Login
+                </Link>
+              </li>
+            )}
 
-            {login &&  <li>
-              <Link
-                to="/sign-up"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Logout
-              </Link>
-            </li>}
+            {login && (
+              <li>
+                <Link
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                 {user}
+                </Link>
+              </li>
+            )}
           </ul>
-          {button && !login && <Button buttonStyle="btn--outline">LOGIN</Button>}
-          {button && login &&  <Button buttonStyle="btn--outline">LOGOUT</Button> }
+          {button && !login && (
+            <Button buttonStyle="btn--outline">LOGIN</Button>
+          )}
+          {button && login && (
+            <Button buttonStyle="btn--outline">
+              {
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                  >
+                    <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" fill="#FFFFFF"/>
+                  </svg>
+                  <span style={{ marginLeft: '8px' }}>{user}</span>
+                </div>
+              }
+            </Button>
+          )}
         </div>
       </nav>
     </>
